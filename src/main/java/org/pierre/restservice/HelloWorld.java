52576c0b4460c437a4643f26a1084b148597f8d1
vernetto <publicpierre@gmail.com>
2019-05-08 03:55:57 +0200

    http://10.0.75.1:7001/javaeetests-1.0-SNAPSHOT/api/employees/2

--- src/main/java/org/pierre/restservice/HelloWorld.java
+++ src/main/java/org/pierre/restservice/HelloWorld.java
@@ -3,17 +3,21 @@ package org.pierre.restservice;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
- * http://localhost:7001/javaeetests-1.0-SNAPSHOT/api/helloworld
+ *
+ * Inspired by this article https://www.baeldung.com/jersey-rest-api-with-spring
+ * Endpoint is http://10.0.75.1:7001/javaeetests-1.0-SNAPSHOT/api/employees/2
  */
 
 
-@Path("helloworld")
-
+@Path("/employees")
 public class HelloWorld {
     @Context
     private UriInfo context;
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYYMMDD:HHmmss");
 
     /**
      * Creates a new instance of HelloWorld
@@ -26,16 +30,13 @@ public class HelloWorld {
      *
      * @return an instance of java.lang.String
      */
-    @GET
+
     @Produces("text/html")
-    public String getHtml() {
-        System.out.println("getHtml");
+    @GET
+    @Path("/{id}")
+    public String getEmployee(@PathParam("id") int id) {
+        System.out.println("getEmployee " + id);
         return "<html lang=\"en\"><body><h1>Hello, World!!</h1></body></html>";
     }
 
-    @POST
-    @Consumes("text/plain")
-    public void postHtml(String message) {
-        System.out.println("postHtml " + message);
-    }
 }
